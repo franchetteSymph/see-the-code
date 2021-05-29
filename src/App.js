64cddea1a07c188a0dff5694c25d809a0965ce5a
Franchette Camoro <franchette.camoro@onevigor.tv>
2021-05-29 14:06:04 +0800

    Update firebase config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Button from "@material-ui/core/Button";
-// import { FirestoreProvider } from "@react-firebase/firestore";
+import { FirestoreProvider } from "@react-firebase/firestore";
+import {config} from './config/firebase-config'
+import firebase from 'firebase'
 
 function App() {
   return (
-    <div>
-      <CssBaseline />
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    </div>
+    <FirestoreProvider {...config} firebase={firebase} >
+      <div>
+        <CssBaseline />
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
+    </FirestoreProvider>
   );
 }
 
